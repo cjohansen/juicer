0f697b8429c2d3d4e64677e0b5ac07d5bd9c6461
Christian Johansen <christian@cjohansen.no>
2008-12-18 00:14:00 +0100

    Commented out test data deletion code that wasn't working anyway

--- test/test_juicer.rb
+++ test/test_juicer.rb
@@ -1,4 +1,4 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "test_helper")) unless defined?(Juicer)
 
-file_setup = Juicer::Test::FileSetup.new($DATA_DIR)
-file_setup.delete!
+#file_setup = Juicer::Test::FileSetup.new($DATA_DIR)
+#file_setup.delete!
