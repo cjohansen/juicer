be9fe9f93aac2d8bc5f03b45df7c9517ec54de19
Christian Johansen <christian@cjohansen.no>
2008-12-19 13:23:32 +0100

    Added options for minifyer as well as forced overwrite.

--- lib/juicer/command/merge.rb
+++ lib/juicer/command/merge.rb
@@ -11,52 +11,35 @@ module Juicer
       #
       def initialize
         super('merge', false, true)
+        @types = { :js => Juicer::Merger::JavaScriptFileMerger,
+                   :css => Juicer::Merger::CssFileMerger }
         @output = nil
+        @force = false
+        @minifyer = "yui_compressor"
         @opts = {}
         self.short_desc = "Combines and minifies CSS and JavaScript files"
         self.description = <<-EOF
-Combines and minifies CSS and JavaScript files. Each file provided as input will
-be checked for dependencies to other files, and those files will be added to
-the final output
+Each file provided as input will be checked for dependencies to other files,
+and those files will be added to the final output
 
-For CSS files the dependency checking is done through @import statements. For
-JavaScript files you can tell Juicer about dependencies through special comment
-switches. These should appear inside a multi-line comment, specifically inside
-the first multi-line comment. The switch is @depend or @depends, your choice.
+For CSS files the dependency checking is done through regular @import
+statements.
 
-Given a file test.js that looks like:
+For JavaScript files you can tell Juicer about dependencies through special
+comment switches. These should appear inside a multi-line comment, specifically
+inside the first multi-line comment. The switch is @depend or @depends, your
+choice.
 
-/**
- * Test JavaScript
- *
- * @depend lib.js
- */
-var Test = {
-    version: "1.0"
-};
-
-and the file lib.js:
-
-/**
- * This is lib.js
- */
-var tools = {
-    dom: {}
-};
-
-Running
-juicer merge test.js
-
-Will result in the file test.min.js containing:
-
-var Test={version:"1.0"};var tools={dom:{}}
-
-Ready for production use. The dependencies nest indefinately.
+The -m --minifyer switch can be used to select which minifyer to use. Currently
+only YUI Compressor is supported, ie -m yui_compressor (default). When using
+the YUI Compressor the path should be the path to where the jar file is found.
         EOF
 
         self.options = CmdParse::OptionParserWrapper.new do |opt|
           opt.on( '-o', '--output [OUTPUT]', 'Output filename' ) { |filename| @output = filename }
-          opt.on( '-b', '--bin-path [BIN_PATH]', 'Path to YUI Compressor jar' ) { |path| @opts[:bin_path] = path }
+          opt.on( '-p', '--path [PATH]', 'Path to compressor binary' ) { |path| @opts[:bin_path] = path }
+          opt.on( '-m', '--minifyer [MINIFYER]', 'Which minifer to use. Currently only supports YUI Compressor' ) { |name| @minifyer = name }
+          opt.on( '-f', '--force', 'Force overwrite of target file' ) { @force = true }
         end
       end
 
@@ -67,20 +50,36 @@ Ready for production use. The dependencies nest indefinately.
           raise OptionParser::ParseError.new('Please provide atleast one input file')
         end
 
+        # If no file name is provided, use name of first input with .min
+        # prepended to suffix
         @output = @output || args[0].sub(/\.([^\.]+)$/, '.min.\1')
         type = $1
-        mergefile = File.join(Dir::tmpdir, Time.new.to_i.to_s + @output)
 
-        merger = type == 'js' ? Juicer::Merger::JavaScriptFileMerger.new : Juicer::Merger::CssFileMerger.new
+        if File.exists?(@output) && !@force
+          puts "Unable to continue, #{@output} exists. Run again with --force to overwrite"
+          exit
+        end
+
+        # Merge files
+        mergefile = File.join(Dir::tmpdir, Time.new.to_i.to_s + @output)
+        merger = @types[type.to_sym].new
         merger << args
         merger.save(mergefile)
-puts merger.save
-puts mergefile
-`cat #{mergefile}`
-        compressor = Juicer::Minifyer::YuiCompressor.new(@opts)
-        compressor.compress(mergefile, @output)
+
+        # Minify
+        begin
+          minifyer = @minifyer.split("_").collect { |p| p.capitalize! }.join
+          compressor = Juicer::Minifyer.const_get(minifyer).new(@opts)
+          compressor.compress(mergefile, @output)
+        rescue
+          puts "No such minifyer '#{minifyer}', aborting"
+          exit
+        end
+
+        # Clean up
         File.delete(mergefile)
 
+        # Print report
         puts "Produced #{@output}"
       end
     end
