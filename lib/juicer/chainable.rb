d088766009c8b0d7de0cbcd98292f041a099411a
Christian Johansen <christian@cjohansen.no>
2009-01-03 23:53:10 +0100

    Added Chainable module to support command chains

---
+++ lib/juicer/chainable.rb
@@ -0,0 +1,105 @@
+module Juicer
+  #
+  # Facilitates the chain of responsibility pattern. Wraps given methods and
+  # calls them in a chain.
+  #
+  # To make an object chainable, simply include the module and call the class
+  # method chain_method for each method that should be chained.
+  #
+  # Example is a simplified version of the Wikipedia one
+  # (http://en.wikipedia.org/wiki/Chain-of-responsibility_pattern)
+  #
+  #  class Logger
+  #    include Juicer::Chainable
+  #
+  #    ERR = 3
+  #    NOTICE = 5
+  #    DEBUG = 7
+  #
+  #    def initialize(level)
+  #      @level = level
+  #    end
+  #
+  #    def log(str, level)
+  #      if level <= @level
+  #        write str
+  #      else
+  #        abort_chain
+  #      end
+  #    end
+  #
+  #    def write(str)
+  #      puts str
+  #    end
+  #
+  #    chain_method :message
+  #  end
+  #
+  #  class EmailLogger < Logger
+  #    def write(str)
+  #      p "Logging by email"
+  #      # ...
+  #    end
+  #  end
+  #
+  #  logger = Logger.new(Logger::NOTICE)
+  #  logger.next_in_chain = EmailLogger.new(Logger::ERR)
+  #
+  #  logger.log("Some message", Logger::DEBUG) # Ignored
+  #  logger.log("A warning", Logger::NOTICE)   # Logged to console
+  #  logger.log("An error", Logger::ERR)       # Logged to console and email
+  #
+  module Chainable
+
+    #
+    # Add the chain_method to classes that includes the module
+    #
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    #
+    # Sets the next command in the chain
+    #
+    def next_in_chain=(next_obj)
+      @_next_in_chain = next_obj
+    end
+
+    alias_method :set_next, :next_in_chain=
+
+    #
+    # Get next command in chain
+    #
+    def next_in_chain
+      @_next_in_chain = nil unless @_next_in_chain
+      @_next_in_chain
+    end
+
+   private
+    #
+    # Abort the chain for the current message
+    #
+    def abort_chain
+      @_abort_chain = true
+    end
+
+    module ClassMethods
+      #
+      # Sets up a method for chaining
+      #
+      def chain_method(method)
+        original_method = "#{method}_orig".to_sym
+        alias_method original_method, method
+
+        self.class_eval <<-RUBY
+          def #{method}(*args, &block)
+            @_abort_chain = false
+            #{original_method}(*args, &block)
+            next_in_chain.#{method}(*args, &block) if !@_abort_chain && next_in_chain
+            @_abort_chain = false
+          end
+        RUBY
+      end
+    end
+  end
+end
---
+++ test/juicer/test_chainable.rb
@@ -0,0 +1,84 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. test_helper])) unless defined?(Juicer)
+
+class Host
+  include Juicer::Chainable
+
+  def initialize(msg = nil)
+    @msg = msg || ""
+  end
+
+  def dummy(ios)
+    ios.print @msg
+  end
+
+  chain_method :dummy
+
+  def unchained(ios)
+    ios.print @msg
+  end
+
+  def abortable(ios, abort = false)
+    ios.print @msg
+    abort_chain if abort
+  end
+
+  chain_method :abortable
+end
+
+class TestChainable < Test::Unit::TestCase
+  def test_next_initial_state
+    host = Host.new
+    assert host.respond_to?(:next_in_chain), "Host should respond to next_in_chain()"
+    assert_nil host.next_in_chain, "Next command should be nil for newly created object"
+  end
+
+  def test_set_next
+    host = Host.new
+    host2 = Host.new
+    host.next_in_chain = host2
+    assert_equal host2, host.next_in_chain
+
+    host3 = host.next_in_chain = Host.new
+    assert_not_equal host3, host
+    assert_not_equal host3, host2
+    assert_equal host3, host.next_in_chain
+  end
+
+  def test_set_next_return_value
+    host = Host.new
+    host2 = host.set_next(Host.new)
+
+    assert_not_equal host2, host
+    assert_equal host2, host.next_in_chain
+  end
+
+  def test_simple_chain
+    host = Host.new("a")
+    host2 = host.next_in_chain = Host.new("b")
+    ios = StringIO.new
+    host.dummy(ios)
+
+    assert_equal "ab", ios.string
+  end
+
+  def test_unchained_method
+    host = Host.new("a")
+    host2 = host.next_in_chain = Host.new("b")
+    ios = StringIO.new
+    host.unchained(ios)
+
+    assert_equal "a", ios.string
+  end
+
+  def test_abort_chain
+    host = Host.new("a")
+    host2 = host.next_in_chain = Host.new("b")
+    ios = StringIO.new
+
+    host.abortable(ios)
+    assert_equal "ab", ios.string
+
+    host.abortable(ios, true)
+    assert_equal "aba", ios.string
+  end
+end
