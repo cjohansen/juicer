f06d6c4e70542a87a9723e068cb24d07efac6469
Christian Johansen <christian@cjohansen.no>
2009-01-13 00:19:19 +0100

    Added JsLint installer and tests

---
+++ lib/juicer/install/jslint_installer.rb
@@ -0,0 +1,50 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. juicer])) unless defined?(Juicer)
+require "zip/zip"
+
+module Juicer
+  module Install
+    #
+    # Install and uninstall routines for the JSLint library by Douglas Crockford.
+    # Installation downloads the jslintfull.js and rhino.js files and stores
+    # them in the Juicer installation directory.
+    #
+    class JSLintInstaller < Base
+      attr_reader :latest
+
+      def initialize(install_dir = Juicer.home)
+        super(install_dir)
+        @latest = "1.0"
+        @website = "http://www.jslint.com/"
+        @path = "lib/jslint"
+        @name = "JsLint"
+        dependency :rhino
+      end
+
+      #
+      # Install JSLint. Downloads the two js files and stores them in the
+      # installation directory.
+      #
+      def install(version = nil)
+        version = super(version)
+        filename = download(File.join(@website, "fulljslint.js"))
+        File.copy(filename, File.join(@install_dir, path, "bin", "jslint-#{version}.js"))
+      end
+
+      #
+      # Uninstalls JSLint
+      #
+      def uninstall(version = nil)
+        super(version) do |dir, version|
+          File.delete(File.join(dir, "bin", "jslint-#{version}.js"))
+        end
+      end
+    end
+
+    #
+    # This class makes it possible to do Juicer.install("jslint") instead of
+    # Juicer.install("j_s_lint"). Sugar, sugar...
+    #
+    class JslintInstaller < JSLintInstaller
+    end
+  end
+end
