784bc47059825341af70e3e78e200763a5589c5a
Christian Johansen <christian@cjohansen.no>
2009-01-12 23:58:30 +0100

    Added Rhino installer

---
+++ lib/juicer/install/rhino_installer.rb
@@ -0,0 +1,50 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. juicer])) unless defined?(Juicer)
+require "zip/zip"
+
+module Juicer
+  module Install
+    #
+    # Install and uninstall routines for the Mozilla Rhino jar.
+    #
+    class RhinoInstaller < Base
+      attr_reader :latest
+
+      def initialize(install_dir = Juicer.home)
+        super(install_dir)
+        @latest = "1_7R2-RC1"
+        @website = "ftp://ftp.mozilla.org/pub/mozilla.org/js/"
+      end
+
+      #
+      # Install Rhino. Downloads the jar file and stores it in the installation
+      # directory along with the License text.
+      #
+      def install(version = nil)
+        version = super((version || latest).gsub(/\./, "_"))
+        base = "rhino#{version}"
+        filename = download(File.join(@website, "#{base}.zip"))
+        target = File.join(@install_dir, path)
+
+        Zip::ZipFile.open(filename) do |file|
+          FileUtils.mkdir_p(File.join(target, version))
+          begin
+            file.extract("#{base.sub(/-RC\d/, "")}/LICENSE.txt", File.join(target, version, "LICENSE.txt"))
+          rescue Exception
+            # Fail silently, some releases don't carry the license
+          end
+          file.extract("#{base.sub(/-RC\d/, "")}/js.jar", File.join(target, "bin", "#{base}.jar"))
+        end
+      end
+
+      #
+      # Uninstalls Rhino
+      #
+      def uninstall(version = nil)
+        super((version || latest).gsub(/\./, "_")) do |dir, version|
+          base = "rhino#{version}"
+          File.delete(File.join(dir, "bin/", "#{base}.jar"))
+        end
+      end
+    end
+  end
+end
---
+++ test/juicer/install/test_rhino_installer.rb
@@ -0,0 +1,56 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. test_helper])) unless defined?(Juicer)
+
+class TestRhinoInstaller < Test::Unit::TestCase
+  def setup
+    @installer = Juicer::Install::RhinoInstaller.new(path(".juicer"))
+  end
+
+  def teardown
+    FileUtils.rm_rf(path(".juicer/lib")) if File.exists?(path(".juicer/lib"))
+  end
+
+  def test_check_installation
+    assert !@installer.installed?
+  end
+
+  def test_install_should_download_jar_and_license
+    @installer.install
+
+    assert File.exists?(path(".juicer"))
+    assert File.exists?(path(".juicer/lib"))
+    assert File.exists?(path(".juicer/lib/rhino"))
+    assert File.exists?(path(".juicer/lib/rhino/bin"))
+    assert_match(/rhino\d\_\d[^\.]*\.jar/, Dir.glob(path(".juicer/lib/rhino/bin/*"))[0])
+
+    files = Dir.glob(path(".juicer/lib/rhino/*_*/**/*"))
+    assert_equal ["LICENSE.txt"], files.collect { |file| file.split("/").pop }.sort
+  end
+
+  def test_uninstall_should_remove_all_files_and_empty_directories
+    @installer.install
+    assert File.exists?(path(".juicer/lib/rhino/bin"))
+    assert_equal 2, Dir.glob(path(".juicer/lib/rhino/**/*")).find_all { |f| File.file?(f) }.length
+
+    @installer.uninstall
+    assert !File.exists?(path(".juicer/lib/rhino"))
+  end
+
+  def test_install_specific_version
+    @installer.install("1.7R2-RC1")
+
+    assert File.exists?(path(".juicer/lib/rhino/bin"))
+    assert File.exists?(path(".juicer/lib/rhino/1_7R2-RC1"))
+    assert_equal "rhino1_7R2-RC1.jar", File.basename(Dir.glob(path(".juicer/lib/rhino/bin/*"))[0])
+  end
+
+  def test_uninstall_should_leave_directories_when_other_versions_are_installed
+    @installer.install
+    @installer.install("1.7R1")
+    assert File.exists?(path(".juicer/lib/rhino/bin"))
+    assert_equal 3, Dir.glob(path(".juicer/lib/rhino/**/*")).find_all { |f| File.file?(f) }.length
+
+    @installer.uninstall("1.7R2-RC1")
+    assert File.exists?(path(".juicer/lib/rhino"))
+    assert_equal 1, Dir.glob(path(".juicer/lib/rhino/**/*")).find_all { |f| File.file?(f) }.length
+ end
+end
