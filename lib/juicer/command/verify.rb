c712511e70837d1497fab4d75ac0f0b7269febe3
Christian Johansen <christian@cjohansen.no>
2009-01-19 00:17:32 +0100

    Added JsLint command veridy

---
+++ lib/juicer/command/verify.rb
@@ -0,0 +1,52 @@
+require File.join(File.dirname(__FILE__), "util")
+require "rubygems"
+require "cmdparse"
+require "pathname"
+
+module Juicer
+  module Command
+    # Verifies problem-free-ness of source code (JavaScript and CSS)
+    #
+    class Verify < CmdParse::Command
+      include Juicer::Command::Util
+
+      # Initializes command
+      #
+      def initialize(log = nil)
+        super('verify', false, true)
+        @log = log || Logger.new(STDIO)
+        self.short_desc = "Verifies that the given JavaScript/CSS file is problem free"
+        self.description = <<-EOF
+Uses JsLint (http://www.jslint.com) to check that code adheres to good coding
+practices to avoid potential bugs, and protect against introducing bugs by
+minifying.
+        EOF
+      end
+
+      # Execute command
+      #
+      def execute(args)
+        # Need atleast one file
+        raise ArgumentError.new('Please provide atleast one input file/pattern') if args.length == 0
+
+        jslint = Juicer::JsLint.new(:bin_path => Juicer.home)
+
+        # Check that JsLint is installed
+        raise FileNotFoundError.new("Missing 3rd party library JsLint, install with\njuicer install jslint") if jslint.locate_lib.nil?
+
+        # Verify all files
+        files(args).each do |file|
+          @log.info "Verifying #{file} with JsLint"
+          report = jslint.check(file)
+
+          if report.ok?
+            @log.info "  OK!"
+          else
+            @log.warn "  Problems detected"
+            @log.warn "  #{report.errors.join("\n").gsub(/\n/, "\n  ")}\n"
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/juicer/command/test_verify.rb
@@ -0,0 +1,57 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. test_helper])) unless defined?(Juicer)
+
+class TestVerifyCommand < Test::Unit::TestCase
+
+  def setup
+    @io = StringIO.new
+    @command = Juicer::Command::Verify.new(Logger.new @io)
+
+    File.open(path("ok.js"), "w") do |file|
+      js = <<-JS
+function hey() {
+    alert("Hey");
+}
+      JS
+
+      file.puts js
+    end
+
+    File.open(path("not-ok.js"), "w") do |file|
+      js = <<-JS
+var a = 34
+b = 78;
+      JS
+
+      file.puts js
+    end
+  end
+
+  def teardown
+    File.delete(path("ok.js"))
+    File.delete(path("not-ok.js"))
+  end
+
+  def test_no_files
+    assert_raise ArgumentError do
+      @command.execute []
+    end
+  end
+
+  def test_installer_not_found
+    Juicer.home = path("somewhere")
+
+    assert_raise FileNotFoundError do
+      @command.execute path("a.js")
+    end
+  end
+
+  def test_verify_several_files
+    Juicer.home = path(".juicer")
+    installer = Juicer::Install::JSLintInstaller.new(path(".juicer"))
+    installer.install unless installer.installed?
+
+    @command.execute([path("ok.js"), path("not-ok.js"), path("a.js")])
+    assert_match /OK!/, @io.string
+    assert_match /Problems detected/, @io.string
+  end
+end
