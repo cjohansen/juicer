8affa1375aa7f306c16328c305153c9e07c71947
Christian Johansen <christian@cjohansen.no>
2009-07-24 00:00:57 +0200

    Use shoulda for core extensions tests

---
+++ test/juicer/ext/test_symbol.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. test_helper])) unless defined?(Juicer)
+
+class TestSymbolExtensions < Test::Unit::TestCase
+  context "camel case method" do
+    should "return camel cased string from underscored symbol" do
+      assert_equal "CamelCase", :camel_case.camel_case
+    end
+
+    should "return camel cased string from upper cased underscored symbol" do
+      assert_equal "CamelCase", :CAMEL_CASE.camel_case
+    end
+
+    should "return camel cased string from 'camel cased' underscored symbol" do
+      assert_equal "CamelCase", :Camel_Case.camel_case
+    end
+  end
+
+  context "classify method" do
+    should "return nested class" do
+      assert_equal Juicer::DependencyResolver, :dependency_resolver.classify(Juicer)
+    end
+
+    should "return top level class from underscored symbol" do
+      assert_equal FileUtils, :file_utils.classify
+    end
+  end
+end
