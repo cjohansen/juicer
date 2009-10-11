3c5858c7ba95c35f846b2825c17c91fb80dbc688
MRoderick <morgan@roderick.dk>
2009-10-11 12:36:59 +0200

    Renaming test files to make rake test discover them

---
+++ test/juicer/image_embed_test.rb
@@ -0,0 +1,70 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. test_helper])) unless defined?(Juicer)
+
+class TestImageEmbed < Test::Unit::TestCase
+  def setup
+    Juicer::Test::FileSetup.new.create
+    @embedder = Juicer::ImageEmbed.new
+  end
+
+  def teardown
+    Juicer::Test::FileSetup.new.delete
+  end
+
+  def test_does_not_modify_regular_path
+    path = 'marker.png'
+    assert_equal( path, @embedder.embed( path ) )
+  end
+
+  def test_does_not_modify_path_flagged_as_not_embeddable
+    path = '/somepath/somefile.png?embed=false'
+    assert_equal( path, @embedder.embed( path ) )
+  end
+
+  def test_should_embed_image_when_path_flagged_as_embeddable
+    path = path( 'images/1.png?embed=true' )
+    assert_not_equal( path, @embedder.embed( path ) )
+  end
+
+  def test_should_embed_png_gif_jpg_jpeg_images
+    path = path( 'images/test_image.png?embed=true' )
+    assert_not_equal( path, @embedder.embed( path ) )
+
+    path = path( 'images/test_image.gif?embed=true' )
+    assert_not_equal( path, @embedder.embed( path ) )
+
+    path = path( 'images/test_image.jpg?embed=true' )
+    assert_not_equal( path, @embedder.embed( path ) )
+
+    path = path( 'images/test_image.jpeg?embed=true' )
+    assert_not_equal( path, @embedder.embed( path ) )
+  end
+
+  def test_should_not_embed_unsupported_filetypes
+    path = '/somepath/somefile.js?embed=true'
+    assert_equal( path, @embedder.embed( path ) )
+
+    path = '/somepath/somefile.swf?embed=true'
+    assert_equal( path, @embedder.embed( path ) )
+
+    path = '/somepath/somefile.ico?embed=true'
+    assert_equal( path, @embedder.embed( path ) )
+
+    path = '/somepath/somefile.bmp?embed=true'
+    assert_equal( path, @embedder.embed( path ) )
+  end
+
+  def test_should_set_correct_mimetype
+    path = path( 'images/test_image.png?embed=true' )
+    assert_match(/image\/png/, @embedder.embed( path ) )
+
+    path = path( 'images/test_image.gif?embed=true' )
+    assert_match(/image\/gif/, @embedder.embed( path ) )
+
+    path = path( 'images/test_image.jpg?embed=true' )
+    assert_match(/image\/jpg/, @embedder.embed( path ) )
+
+    path = path( 'images/test_image.jpeg?embed=true' )
+    assert_match(/image\/jpeg/, @embedder.embed( path ) )
+  end
+
+end
