86dd9d8b7f828985e489cc703001a9ba17034f0f
Christian Johansen <christian@shortcut.no>
2009-11-25 00:05:24 +0100

    Fix dated juicer test file

--- test/unit/juicer_test.rb
+++ test/unit/juicer_test.rb
@@ -1,5 +1,1 @@
-require File.expand_path(File.join(File.dirname(__FILE__), "test_helper")) unless defined?(Juicer)
-
-#file_setup = Juicer::Test::FileSetup.new($DATA_DIR)
-#file_setup.delete!
-#Juicer::Test.setup
+require "test_helper"
