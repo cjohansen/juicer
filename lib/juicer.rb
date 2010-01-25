0dbdc9e97669a39a57ae3d470e9d6091dafbe417
Christian Johansen <christian@shortcut.no>
2010-01-25 13:02:56 +0100

    Display version number as 1.0.0-A2

--- lib/juicer.rb
+++ lib/juicer.rb
@@ -3,7 +3,7 @@ require "logger"
 module Juicer
 
   # :stopdoc:
-  VERSION = '1.0.0-a1'
+  VERSION = '1.0.0-a2'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   LOGGER = Logger.new(STDOUT)
