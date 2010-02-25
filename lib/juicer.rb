fb35aa8b162a06d5fa2c9dbed528dcd57cf8b80d
Christian Johansen <christian@shortcut.no>
2010-02-26 00:06:29 +0100

    Release 1.0.0

--- lib/juicer.rb
+++ lib/juicer.rb
@@ -3,7 +3,7 @@ require "logger"
 module Juicer
 
   # :stopdoc:
-  VERSION = '1.0.0-a2'
+  VERSION = '1.0.0'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   LOGGER = Logger.new(STDOUT)
