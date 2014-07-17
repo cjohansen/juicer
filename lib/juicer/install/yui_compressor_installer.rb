a328f686228490b4ad9417ee9aa73eb355128a6c
Rolf Bjaanes <rolf@powow.no>
2014-07-17 14:49:16 +0200

    Update cdn for Yui Compressor, fixes #76

--- lib/juicer/install/yui_compressor_installer.rb
+++ lib/juicer/install/yui_compressor_installer.rb
@@ -16,7 +16,7 @@ module Juicer
         @latest = nil
         @href = nil
         @website = 'https://github.com/yui/yuicompressor/downloads'
-        @cdn = 'http://cloud.github.com'
+        @cdn = 'https://github.com'
       end
 
       #
